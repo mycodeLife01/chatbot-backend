edaf19a8d62cfa023c2a733f5ea0599dc588b51a
JackHomeSh <dpj0225@gmail.com>
2025-05-22 11:56:19 +0800

    first commit

---
+++ app/dependencies/db.py
@@ -0,0 +1,11 @@
+from sqlmodel import create_engine, Session
+from typing import Generator
+
+DATABASE_URL = "mysql+pymysql://localhost:3306/chatbot"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
