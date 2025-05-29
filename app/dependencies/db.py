9bf765d6fe448249a10b7c2e70b84aee0534c159
jam <dpj0225@gmail.com>
2025-05-29 11:21:55 +0800

    swicth db to cloud

--- app/dependencies/db.py
+++ app/dependencies/db.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine, Session
 from typing import Generator
 
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/chatbot"
+DATABASE_URL = "mysql+pymysql://root:Dpj89757!@47.116.194.240:3306/chatbot"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
